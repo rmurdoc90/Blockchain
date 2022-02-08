b9be86f804d6c36f754d1d5dee9c2c19d3aa5f0b
rmurdoc90 <rmurdoc90@gmail.com>
2022-02-08 09:52:36 -0800

    building out deploy.js

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,10 @@
-// deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "project orphan need scene snake ivory year icon priority cute trophy hockey",
+  "https://rinkeby.infura.io/v3/3b1e4a87ae974414adbd85a0ed6b683e"
+);
+
+const web3 = new Web3(provider);
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -11,6 +11,8 @@ const web3 = new Web3(ganache.provider());
 let accounts;
 let inbox;
 
+//
+
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
@@ -21,31 +23,17 @@ beforeEach(async () => {
 
 describe("Inbox", () => {
   it("deployed a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "bye");
   });
 });
-
-// class Car {
-//   park() {
-//     return "stopped";
-//   }
-//   drive() {
-//     return "vroom";
-//   }
-// }
-
-// let car;
-
-// beforeEach(()=>{
-//   car = new Car();
-// })
-
-// describe("Car", () => {
-//   it("can park", () => {
-//     assert.equal(car.park(), "stopped");
-//   });
-
-//   it("can drive", () => {
-//     assert.equal(car.drive(), "vroom");
-//   });
-// });
