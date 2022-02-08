171d8199d0e6ff6d865d354ce5432b22dd757e72
rmurdoc90 <rmurdoc90@gmail.com>
2022-02-07 16:32:18 -0800

    fake tests added

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,1 +1,22 @@
-// contract test code will go here
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+
+//create instance of new web3
+const web3 = new Web3(ganache.provider());
+
+class Car {
+  park() {
+    return "stopped";
+  }
+  drive() {
+    return "vroom";
+  }
+}
+
+describe("Car", () => {
+  it("can park", () => {
+    const car = new Car();
+    assert.equal(car.park(), "stoooopped");
+  });
+});
