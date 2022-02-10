ae7ccac6e3576c9cf2f602994bb8533552887efa
rmurdoc90 <rmurdoc90@gmail.com>
2022-02-10 10:09:20 -0800

    lottery dir made

---
+++ Lottery/contracts/Lottery.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+contract Lottery{
+    address public manager;
+    address[] public players;
+
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+      return uint(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+
+    modifier restricted(){
+        require(manager == msg.sender);
+        _;
+    }
+    function getPlayers() public view returns (address[] memory){
+        return players;
+    }
+    
+}
---
+++ Lottery/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "project orphan need scene snake ivory year icon priority cute trophy hockey",
+  "https://rinkeby.infura.io/v3/3b1e4a87ae974414adbd85a0ed6b683e"
+);
+
+const web3 = new Web3(provider);
+
+const desploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+
+  provider.engine.stop();
+};
+
+desploy();
